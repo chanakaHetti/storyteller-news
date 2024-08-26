bef53e4b51915ecfb520a05700b1732efd8c2c7f
chanakaHetti <cdileep40@gmail.com>
2024-08-26 16:12:01 +0100

    Building the latest story UI with dummy data

--- src/components/latest-story.jsx
+++ src/components/latest-story.jsx
@@ -2,21 +2,13 @@ import React from 'react';
 
 import { timeCalculation } from '@/helpers/time-calculation';
 
-// source: {
-//   id: 'google-news',
-//   name: 'Google News',
-// },
-// author: 'Xataka',
-// title:
-//   'Nuevo SUV de Xiaomi: fecha de lanzamiento, precio, modelos y todo lo que creemos saber sobre su próximo... - Xataka',
-// description: null,
-// url: 'https://news.google.com/rss/articles/CBMibkFVX3lxTFB4bTdQcDA5Q2tWd25FUWJpVnR1VGVsMTA2VEoxaTR0Rzl0S1BuSGQ5RmtNdFI3ckJJQnlzNTRZOWc2aHVXeFk5NlRSQjBSZVFVby02ektDSVNKUk4zWThvV1JZWDd1TVMyaTVXTzFn0gFzQVVfeXFMTlR1MnhRTHBCdUtLai0yc2EyRUZYMlB2eEd0M19haUk5c0tocnZ1ZnV0SzdPa0hYSk4zNk5lckpMU2Y0Y1VDZkNQd1RObnJzeHpjSEhJcy1CNnFiRlRVXzN0M3FsZHpvSHhMd0k2TjVKa3NUYw?oc=5',
-// urlToImage: null,
-// publishedAt: '2024-08-25T12:30:27Z',
-// content: null,
-
 const LatestStory = ({ story }) => {
   const { title, source, content, urlToImage, publishedAt } = story;
+
+  const newsContent = `${
+    content ||
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. has been the industrys standard dummy text ever since'
+  }`;
 
   return (
     <div className="latest-story">
@@ -28,7 +20,7 @@ const LatestStory = ({ story }) => {
       />
       <div className="latest-story__source">{source.name}</div>
       <h2 className="latest-story__title">{title}</h2>
-      <p className="latest-story__summary">{content}</p>
+      <p className="latest-story__summary">{newsContent}</p>
       <div className="latest-story__time">{timeCalculation(publishedAt)}</div>
     </div>
   );
